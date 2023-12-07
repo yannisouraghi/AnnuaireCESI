9e3bf55aea6b57f8065e11f7c3fc593104006200
Ouraghi Yannis <85176113+yannisouraghi@users.noreply.github.com>
2023-12-07 11:47:50 +0100

    Add files via upload

---
+++ View/Add.xaml.cs
@@ -0,0 +1,62 @@
+﻿using DevExpress.Mvvm.POCO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProjetAnnuaire.View
+{
+    /// <summary>
+    /// Logique d'interaction pour AddService.xaml
+    /// </summary>
+    public partial class Add : Window
+    {
+        //Genere ma fenetre pour fonctionner avec le code de ma page AjouterPageViewModel, qui est la page qui gere les actions de la fenetre et qui appel cette fenetre, en focntion du prompt en parametre on affiche dans la fenetre le bon message, gere aussi la fonction ajouter et annuler de la fenetre
+        public static string UserInput { get; private set; }
+
+
+        public Add(string prompt)
+        {
+            InitializeComponent();
+
+            if (prompt == "Ajouter un service")
+            {
+                LabelContent.Text = "Ajouter un service";
+                UserInput = "";
+            }
+            else if (prompt == "Ajouter un site")
+            {
+                
+                LabelContent.Text = "Ajouter un site";
+                UserInput = "";
+                
+            }
+        }
+
+            private void Add_Click(object sender, RoutedEventArgs e)
+            {
+                if (ServiceInput.Text == "")
+                {
+                    MessageBox.Show("Veuillez saisir une valeur valide");
+                    return;
+                }
+                UserInput = ServiceInput.Text;
+                this.Close();
+            }
+
+            private void Cancel_Click(object sender, RoutedEventArgs e)
+            {
+                UserInput = "";
+                this.Close();
+            }
+        }
+}
---
+++ View/Update.xaml.cs
@@ -0,0 +1,99 @@
+﻿using Newtonsoft.Json;
+using ProjetAnnuaire.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Net.Http;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using ProjetAnnuaire.ViewModel;
+using System.Windows.Input;
+
+namespace ProjetAnnuaire.View
+{
+    /// <summary>
+    /// Logique d'interaction pour AddService.xaml
+    /// </summary>
+    public partial class Update : Window
+    {
+        public static string UserInput { get; private set; }
+        public static int Id { get; private set; }
+        public UpdateViewModel ViewModel { get; set; }
+
+
+        public Update(string prompt)
+        {
+            InitializeComponent();
+            ViewModel = new UpdateViewModel();
+            DataContext = ViewModel;
+
+            if (prompt == "Modifier un service")
+            {
+                InitializeServicesComboBoxAsync();
+                ServiceComboBox.Visibility = Visibility.Visible;
+                LabelContent.Text = "Modifier un service";
+                UserInput = "";
+            }
+            else if (prompt == "Modifier un site")
+            {
+                InitializeSitesComboBoxAsync();
+                SiteComboBox.Visibility = Visibility.Visible;
+                LabelContent.Text = "Modifier un site";
+                UserInput = "";
+
+            }
+        }
+        private async Task InitializeServicesComboBoxAsync()
+        {
+            await ViewModel.InitializeServicesAsync();
+        }
+
+        private async Task InitializeSitesComboBoxAsync()
+        {
+            await ViewModel.InitializeSitesAsync();
+        }
+        
+        private void Add_Click(object sender, RoutedEventArgs e)
+        {
+            if (LabelContent.Text == "Modifier un service" && ServiceComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Veuillez sélectionner un service");
+                return;
+            }
+            else if (LabelContent.Text == "Modifier un site" && SiteComboBox.SelectedItem == null)
+            {
+                MessageBox.Show("Veuillez sélectionner un site");
+                return;
+            }
+            if (ServiceInput.Text == "")
+            {
+                MessageBox.Show("Veuillez saisir une valeur valide");
+                return;
+            }
+            if (LabelContent.Text == "Modifier un service")
+            {
+                var ServiceId = (ServiceComboBox.SelectedItem as Services)?.ServiceId ?? 0;
+                Id = ServiceId;
+            }
+            else if (LabelContent.Text == "Modifier un site")
+            {
+                var SiteId = (SiteComboBox.SelectedItem as Sites)?.SiteId ?? 0;
+                Id = SiteId;
+            }
+            UserInput = ServiceInput.Text;
+            this.Close();
+        }
+
+        private void Cancel_Click(object sender, RoutedEventArgs e)
+        {
+            UserInput = "";
+            this.Close();
+        }
+
+    }
+}
